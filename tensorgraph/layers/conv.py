e3e9e9a6d2260b4a5d01d57f1afcecdf3f5ca664
Wu <hyciswu@gmail.com>
2016-07-07 17:38:20 +0800

    added embedding and reshape layer

--- tensorgraph/layers/conv.py
+++ tensorgraph/layers/conv.py
@@ -57,9 +57,46 @@ class Conv2D(Template):
     def _variables(self):
         return [self.W, self.b]
 
-# TODO
+
 class Conv2D_Transpose(Template):
-    pass
+    def __init__(self, input_channels, filters, output_shape, stride=(1,1),
+                 W=None, b=None, border_mode='SAME'):
+        '''
+        PARAM:
+            input_channels (int)
+            filters (int): output channels
+            output_shape: 2D tuple of (h, w)
+            padding: "SAME" or "VALID"
+        '''
+        self.input_channels = input_channels
+        self.filters = filters
+        self.output_shape = output_shape
+        self.stride = stride
+        self.border_mode = border_mode
+
+        width, height = self.output_shape
+        self.W_shape = (width, height, self.filters, self.input_channels)
+        self.W = W
+        if self.W is None:
+            self.W = tf.Variable(tf.random_normal(self.W_shape, stddev=0.1), name='W')
+
+        self.b = b
+        if self.b is None:
+            self.b = tf.Variable(tf.random_normal([self.filters], stddev=0.1), name='b')
+
+    def _train_fprop(self, state_below):
+        '''
+        state_below: (b, h, w, c)
+        '''
+        batch_size = tf.shape(state_below)[0]
+        deconv_shape = tf.pack((batch_size,) + self.output_shape + (self.filters,))
+        conv_out = tf.nn.conv2d_transpose(state_below, self.W, output_shape=deconv_shape,
+                                          strides=(1,)+self.stride+(1,), padding=self.border_mode)
+        return tf.nn.bias_add(conv_out, self.b)
+
+    @property
+    def _variables(self):
+        return [self.W, self.b]
 
 # TODO
 class AvgPooling(Template):
--- tensorgraph/layers/misc.py
+++ tensorgraph/layers/misc.py
@@ -9,8 +9,17 @@ class Flatten(Template):
         return tf.reshape(state_below, [shape[0], np.prod(shape[1:])])
 
 
+class Reshape(object):
+
+    def __init__(self, shape):
+        self.shape = shape
+
+    def _train_fprop(self, X):
+        return tf.reshape(X, self.shape)
+
+
 class Embedding(Template):
-    def __init__(self, cat_dim=None, encode_dim=None, W=None, include_zero=False):
+    def __init__(self, cat_dim=None, encode_dim=None, W=None):
         """
         DESCRIPTION:
             embedding
@@ -18,24 +27,16 @@ class Embedding(Template):
             cat_dim (int): number of categories
             encode_dim (int): dense encoding of the categories
             W (tensor variable): embedding of 2D tensor matrix
-            include_zero (bool): to include a null embedding row, if true, there
-                will be an additional embedding row (cat_dim+1), index 0 will be
-                for uncategorized features, real embedding starts from index 1.
         """
 
         self.cat_dim = cat_dim
         self.encode_dim = encode_dim
         self.W = W
 
-        if include_zero:
-            if self.W is None:
-                embed = tf.random_normal([self.cat_dim+1, self.encode_dim], stddev=0.1)
-                embed[0] = 0
-                self.W = tf.Variable(embed, name='W')
-        else:
-            if self.W is None:
-                self.W = tf.Variable(tf.random_normal([self.cat_dim, self.encode_dim], stddev=0.1), name='W')
-
+        if self.W is None:
+            embed = tf.random_normal([self.cat_dim, self.encode_dim], stddev=0.1)
+            self.W = tf.Variable(embed, name='W')
+            
 
     def _train_fprop(self, state_below):
         '''
