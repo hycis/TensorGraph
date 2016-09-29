0965fda4aaf6cad1f95a5e3ebee95fd84aeb0985
Vinay <rmdort@gmail.com>
2016-09-29 14:19:16 +0800

    Remove datablock in export

--- tensorgraph/__init__.py
+++ tensorgraph/__init__.py
@@ -5,4 +5,4 @@ from sequential import Sequential
 from graph import Graph
 from node import StartNode, HiddenNode, EndNode
 from progbar import ProgressBar
-from data_iterator import SequentialIterator, StepIterator, DataBlocks
+from data_iterator import SequentialIterator, StepIterator
