6980270c3dac73f4a4f03bf3d6e6feb006137b06
Wu <hyciswu@gmail.com>
2016-09-29 13:56:38 +0800

    removed matplotlib dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
                       'six>=1.9.0',
                       'scikit-learn>=0.17',
                       'pandas>=0.17',
-                      'matplotlib>=1.5',
                       'scipy>=0.17'],
     url='https://github.com/hycis/TensorGraph',
     download_url = 'https://github.com/hycis/TensorGraph/tarball/1.4.5',
