35143ff3d5504c1b93c44f739f7849eaaab2da91
hycis <hyciswu@gmail.com>
2016-09-30 06:04:32 +0800

    added sparse_matmul

--- tensorgraph/layers/linear.py
+++ tensorgraph/layers/linear.py
@@ -9,10 +9,10 @@ class Linear(Template):
         DESCRIPTION:
             This is a fully connected layer
         PARAM:
-            prev_dim(int): dimension of previous layer
-            this_dim(int): dimension of this layer
-            W(tensor variable): Weight of 2D tensor matrix
-            b(tensor variable): bias of 2D tensor matrix
+            prev_dim (int): dimension of previous layer
+            this_dim (int): dimension of this layer
+            W (tensor variable): Weight of 2D tensor matrix
+            b (tensor variable): bias of 2D tensor matrix
         """
 
         self.prev_dim = prev_dim
@@ -42,13 +42,13 @@ class LinearMasked(Template):
         DESCRIPTION:
             This is a fully connected layer with an applied mask for partial connections
         PARAM:
-            prev_dim(int): dimension of previous layer
-            this_dim(int): dimension of this layer
-            name(string): name of the layer
-            W(tensor variable): Weight of 2D tensor matrix
-            b(tensor variable): bias of 2D tensor matrix
-            mask(numpy.ndarray or tensorflow placeholder): mask for partial connection
-            params(list): a list of params in layer that can be updated
+            prev_dim (int): dimension of previous layer
+            this_dim (int): dimension of this layer
+            name (string): name of the layer
+            W (tensor variable): Weight of 2D tensor matrix
+            b (tensor variable): bias of 2D tensor matrix
+            mask (numpy.ndarray or tensorflow placeholder): mask for partial connection
+            params (list): a list of params in layer that can be updated
         """
 
         self.prev_dim = prev_dim
@@ -66,6 +66,49 @@ class LinearMasked(Template):
 
     def _train_fprop(self, state_below):
         return tf.mul(tf.matmul(state_below, self.W) + self.b, self.mask)
+
+    @property
+    def _variables(self):
+        return [self.W, self.b]
+
+
+class SparseLinear(Template):
+
+    def __init__(self, prev_dim=None, this_dim=None, ndim=None, W=None, b=None):
+        """
+        DESCRIPTION:
+            This is a fully connected layer with sparse inputs are two tensors
+            one is index tensor of dimension [N, ndim] and another one is value
+            tensor of [N]
+        PARAM:
+            prev_dim (int): dimension of previous layer
+            this_dim (int): dimension of this layer
+            name (string): name of the layer
+            W (tensor variable): Weight of 2D tensor matrix
+            b (tensor variable): bias of 2D tensor matrix
+            params (list): a list of params in layer that can be updated
+        """
+
+        self.prev_dim = prev_dim
+        self.this_dim = this_dim
+        self.mask = mask
+
+        self.W = W
+        if self.W is None:
+            self.W = tf.Variable(tf.random_normal([self.prev_dim, self.this_dim], stddev=0.1),
+                                                   name=self.__class__.__name__ + '_W')
+
+        self.b = b
+        if self.b is None:
+            self.b = tf.Variable(tf.zeros([self.this_dim]), name=self.__class__.__name__ + '_b')
+
+    def _train_fprop(self, state_below):
+        idx, val = state_below
+        # nrow = val.get_shape()[0]
+        X = tf.SparseTensor(idx, val, shape=[-1, self.prev_dim])
+        X = tf.sparse_reorder(X)
+        XW = tf.sparse_tensor_dense_matmul(X, self.W, adjoint_a=False, adjoint_b=False)
+        return tf.sparse_add(XW, self.b)
 
     @property
     def _variables(self):
