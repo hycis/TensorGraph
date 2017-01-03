19edada97402fea0d56746ac4ab18e7ca6c829df
Wu <hyciswu@gmail.com>
2017-01-03 14:26:51 +0800

    updated f1

--- tensorgraph/cost.py
+++ tensorgraph/cost.py
@@ -14,19 +14,19 @@ def entropy(ytrue, ypred):
     return tf.reduce_mean(L)
 
 def binary_f1(ytrue, ypred):
-    r = recall(ytrue, ypred)
-    p = precision(ytrue, ypred)
+    r = binary_recall(ytrue, ypred)
+    p = binary_precision(ytrue, ypred)
     return 2 * p * r / (p + r)
 
 def binary_recall(ytrue, ypred):
-    P = tf.reduce_sum(ytrue[:, 1], 1)
+    P = tf.reduce_sum(ytrue[:, 1])
     TP = tf.reduce_sum(ypred[:,1] * ytrue[:,1]) # both ypred and ytrue are positives
-    return TP / P
+    return tf.to_float(TP) / tf.to_float(P)
 
 def binary_precision(ytrue, ypred):
-    TPnFP = tf.reduce_sum(ypred[:, 1], 1)
+    TPnFP = tf.reduce_sum(ypred[:, 1])
     TP = tf.reduce_sum(ypred[:,1] * ytrue[:,1]) # both ypred and ytrue are positives
-    return TP / TPnFP
+    return tf.to_float(TP) / tf.to_float(TPnFP)
 
 def hingeloss(ytrue, ypred):
     ypred = tf.clip_by_value(ypred, 0., 1.0)
---
+++ test/cost_test.py
@@ -0,0 +1,20 @@
+import tensorgraph as tg
+import tensorflow as tf
+import numpy as np
+from tensorgraph.utils import make_one_hot
+# from tensorgraph.cost import _flatten_arg
+
+
+ph1 = tf.placeholder('int32', [None, 2])
+ph2 = tf.placeholder('int32', [None, 2])
+
+
+f1_sb = tg.cost.binary_f1(ph1, ph2)
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+    y1 = np.random.randint(0, 2, 100)
+    y2 = np.random.randint(0 ,2, 100)
+
+    y1_oh = make_one_hot(y1, 2)
+    y2_oh = make_one_hot(y2, 2)
+    print(sess.run(f1_sb, feed_dict={ph1:y1_oh, ph2:y2_oh}))
