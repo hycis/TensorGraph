215f46cf3ad69d3cb5959459d6f4e5836fbeb94f
Wu <hyciswu@gmail.com>
2017-04-13 14:08:02 +0800

    added depthwise conv2d

--- tensorgraph/layers/conv.py
+++ tensorgraph/layers/conv.py
@@ -91,6 +91,56 @@ class Conv2D(Template):
         return [self.filter, self.b]
 
 
+
+class Depthwise_Conv2D(Template):
+    def __init__(self, input_channels, num_filters, kernel_size=(3,3), stride=(1,1),
+                 filter=None, b=None, padding='VALID'):
+        '''
+        DESCRIPTION:
+            Depthwise 2-D convolution.
+            Given an input tensor of shape [batch, in_height, in_width, in_channels]
+            and a filter tensor of shape [filter_height, filter_width, in_channels, channel_multiplier]
+            containing in_channels convolutional filters of depth 1, depthwise_conv2d
+            applies a different filter to each input channel (expanding from 1 channel
+            to channel_multiplier channels for each), then concatenates the results together.
+            The output has in_channels * channel_multiplier channels.
+
+        PARAM:
+            padding: "SAME" pad_along_height = ((out_height - 1) * stride[0] + filter_height - in_height)
+                            pad_along_width = ((out_width - 1) * stride[1] + filter_width - in_width)
+                      or
+                     "VALID" padding is always 0
+        '''
+        self.input_channels = input_channels
+        self.num_filters = num_filters
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        self.filter_shape = self.kernel_size + (self.input_channels, self.num_filters)
+        self.filter = filter
+        if self.filter is None:
+            self.filter = tf.Variable(tf.random_normal(self.filter_shape, stddev=0.1),
+                                      name=self.__class__.__name__ + '_filter')
+
+        self.b = b
+        if self.b is None:
+            self.b = tf.Variable(tf.zeros([self.num_filters*self.input_channels]),
+                                 name=self.__class__.__name__ + '_b')
+
+    def _train_fprop(self, state_below):
+        '''
+        state_below: (b, h, w, c)
+        '''
+        conv_out = tf.nn.depthwise_conv2d(state_below, self.filter, strides=(1,)+self.stride+(1,),
+                                          padding=self.padding)
+        return tf.nn.bias_add(conv_out, self.b)
+
+    @property
+    def _variables(self):
+        return [self.filter, self.b]
+
+
 class Conv2D_Transpose(Template):
     def __init__(self, input_channels, num_filters, output_shape, kernel_size=(3,3), stride=(1,1),
                  filter=None, b=None, padding='VALID'):
