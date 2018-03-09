0e9b064b4ec75fcc2964b74b8a0f337cf44b532b
hycis <hyciswu@gmail.com>
2018-03-09 10:48:34 +0800

    optimize graph to keep track visited nodes

--- tensorgraph/graph.py
+++ tensorgraph/graph.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+
 class Graph(object):
+    TRAIN_FPROP = 'train_fprop'
+    TEST_FPROP = 'test_fprop'
 
     def __init__(self, start, end):
         '''
@@ -17,6 +20,9 @@ class Graph(object):
             assert node.__class__.__name__ == 'StartNode'
         for node in self.end:
             assert node.__class__.__name__ == 'EndNode'
+        # nodes visited during train or test fprop
+        self.visited_train = {}
+        self.visited_test = {}
 
 
     def _output(self, node, mode):
@@ -28,22 +34,42 @@ class Graph(object):
                 return []
         input_vars = []
         for pnode in node.prev:
-            input_vars += self._output(pnode, mode)
+            if mode == Graph.TRAIN_FPROP:
+                # check if the train mode of hidden node has been visited
+                if pnode not in self.visited_train:
+                    output = self._output(pnode, mode)
+                    input_vars += output
+                    self.visited_train[pnode] = output
+                else:
+                    input_vars += self.visited_train[pnode]
+
+            elif mode == Graph.TEST_FPROP:
+                # check if the test mode of hidden node has been visited
+                if pnode not in self.visited_test:
+                    output = self._output(pnode, mode)
+                    input_vars += output
+                    self.visited_test[pnode] = output
+                else:
+                    input_vars += self.visited_test[pnode]
+            else:
+                raise Exception('unknown mode: {}'.format(mode))
+
         node.input_vars = input_vars
         return getattr(node, mode)()
 
 
     def train_fprop(self):
+        print('using new graph')
         outs = []
         for node in self.end:
-            outs += self._output(node, 'train_fprop')
+            outs += self._output(node, Graph.TRAIN_FPROP)
         return outs
 
 
     def test_fprop(self):
         outs = []
         for node in self.end:
-            outs += self._output(node, 'test_fprop')
+            outs += self._output(node, Graph.TEST_FPROP)
         return outs
 
     @property
