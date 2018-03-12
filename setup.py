c9e784e97dc0bb3d9f69fc787d08cd4cec8e6498
hycis <hyciswu@gmail.com>
2018-03-12 10:03:35 +0800

    4.4.9

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
 from setuptools import find_packages
-# from .tensorgraph import __version__
-# __version__ = '4.4.3'
 import json
 
 with open('./tensorgraph/pkg_info.json') as fp:
@@ -17,8 +15,7 @@ setup(
                       'six>=1.9.0',
                       'scikit-learn>=0.17',
                       'pandas>=0.17',
-                      'scipy>=0.17',
-                      'tensorflow-gpu>=1.2.1'],
+                      'scipy>=0.17'],
     url='https://github.com/hycis/TensorGraph',
     download_url = 'https://github.com/hycis/TensorGraph/tarball/{}'.format(__version__),
     license='Apache 2.0, see LICENCE',
